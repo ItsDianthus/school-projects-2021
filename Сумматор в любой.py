4cd16c5c72bab277775910c71cb87893b1d56ccb
Diana Chade <91020940+ItsDianthus@users.noreply.github.com>
2021-09-21 10:20:13 +0300

    Update Сумматор в любой.py

--- Сумматор в любой.py
+++ Сумматор в любой.py
@@ -5,15 +5,14 @@ rem = 0
 fin = ''
 while a > 0 or b > 0:
     summ = a % 10 + b % 10 + rem
-    if summ > 1:
-        rem = summ // base
-        fin = str(summ % base) + fin
+    # if summ > 1:
+    rem = summ // base
+    fin = str(summ % base) + fin
     b //= 10
     a //= 10
 if rem != 0:
      print(str(rem) + fin)
 else:
     print(fin)
-    
     
     
